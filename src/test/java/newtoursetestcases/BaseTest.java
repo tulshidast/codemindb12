3ddb642e2270cd0eabccfc5552d648d2a27b9060
tulshidast <tulsidast77@gmail.com>
2024-10-02 07:59:28 +0530

    changes due to failure

--- src/test/java/newtoursetestcases/BaseTest.java
+++ src/test/java/newtoursetestcases/BaseTest.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.codemind.b12.Utility;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 
@@ -16,10 +19,10 @@ public class BaseTest {
 	@BeforeMethod(alwaysRun = true)
 	public void setup() throws IOException {
 
-//		WebDriverManager.chromedriver().setup();
-//		driver = new ChromeDriver();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 
-		driver = Utility.getDriver();
+//		driver = Utility.getDriver();
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
