bd404628013a396da8f8b197f574f0dab287643a
tulshidast <tulsidast77@gmail.com>
2024-10-02 07:44:08 +0530

    annotation changes

--- src/test/java/newtoursetestcases/BaseTest.java
+++ src/test/java/newtoursetestcases/BaseTest.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.codemind.b12.Utility;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 
@@ -16,7 +19,8 @@ public class BaseTest {
 	@BeforeMethod(alwaysRun = true)
 	public void setup() throws IOException {
 
-		this.driver = Utility.getDriver();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
